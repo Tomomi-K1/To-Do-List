e6b21a331fb2d76992262c96a62e708622704bfe
Tomomi <31785367+Tomomi-K1@users.noreply.github.com>
2022-06-20 04:27:10 -0700

    updated localStorage

--- app.js
+++ app.js
@@ -1,38 +1,101 @@
-const form = document.querySelector('#newTaskForm');
-const btn = document.querySelector('button[type="submit"]');
-const task = document.querySelector('#task');
+const body = document.querySelector('body');
+const newTaskForm=document.querySelector('#newTaskForm');
 const toDoList = document.querySelector('#toDoList');
-const savedTodos = [];
+const todo = document.querySelector('#todo');
 
-form.addEventListener('submit', function(e){
+const savedTodos = JSON.parse(localStorage.getItem('todos'))||[];
+
+for(let i = 0; i < savedTodos.length; i++){
+    let newText = savedTodos[i].task.split('');
+    let array = newText.splice(-6, 6);
+
+    // // console.log(savedTodos[i].task.split('').slice(-6,6).join(''));
+    // console.log(array, string);
+    // Console.log(savedTodos[i].task.splice(-1, 6));
+    let newItem = document.createElement('li');
+    newItem.innerText = newText.join('');
+    toDoList.append(newItem);
+
+  
+    //create checkbox for done
+    let newCkBox= document.createElement('input');
+    newCkBox.setAttribute('type', 'checkbox');
+    newItem.append(newCkBox);
+    console.log(newCkBox.parentElement)
+    if(savedTodos[i].checked === true){
+        newItem.classList.add('strike');
+        newCkBox.checked = true;
+    } 
+
+    //create remove btn
+    let removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    newItem.append(removeBtn);
+}
+
+newTaskForm.addEventListener('submit', function(e){
+    //prevent default refresh
     e.preventDefault();
-    const newLi = document.createElement('li');
-    const newCkBox= document.createElement('input');
-    const removeBtn = document.createElement('button');
-    if (task.value === ''){
+       
+    //prevent empty string to be added
+    if (todo.value === ''){
         alert('Please enter what you need to do.')
     } else{
-    newCkBox.setAttribute('type', 'checkbox');
-    removeBtn.setAttribute('class', 'remove');
-    newLi.innerText = task.value;
+    
+    //create remove btn
+    let removeBtn = document.createElement('button');
     removeBtn.innerText = 'Remove';
+    
+    //create checkbox for done
+    let newCkBox= document.createElement('input');
+    newCkBox.setAttribute('type', 'checkbox');
+    newCkBox.checked = false;
+        
+    //creating li and checkbox and remove btn
+    let newLi = document.createElement('li');
+    newLi.innerText = todo.value;
+    
+    
+    //append created items
     toDoList.append(newLi);
     newLi.append(newCkBox);
     newLi.append(removeBtn);
+    
+    //save to-do information to an array
+    savedTodos.push({task: newLi.innerText, checked: false});
+    console.log(savedTodos);
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+    //reset form    
+    newTaskForm.reset();
     }
-    form.reset();
-
-    //add to localStorage
-    savedTodos = ({task: task.value, });
-
-})
+});
 
 toDoList.addEventListener('click', function(e){
+    console.log(e.target);
+
     if(e.target.tagName === 'INPUT'){
         e.target.parentElement.classList.toggle('strike');
+        console.log(e.target.parentElement.innerText);
+        if(e.target.checked){
+            for(let i = 0; i< savedTodos.length; i++){
+                if (savedTodos[i].task === e.target.parentElement.innerText){
+                    savedTodos[i].checked = true;
+                    console.log("input", savedTodos);
+                }
+            }
+        } else{
+            savedTodos[i].checked = false;
+        }
     } else if (e.target.tagName === 'BUTTON'){
         e.target.parentElement.remove();
+        for(let i = 0; i< savedTodos.length; i++){
+            if (savedTodos[i].task === e.target.parentElement.innerText){
+                savedTodos.splice(i, 1);
+                console.log(savedTodos);
+            }
+        }
     }
-})
 
-
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+});
